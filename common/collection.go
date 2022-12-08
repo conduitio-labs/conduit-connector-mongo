66a7d84a1cafe7535b001e176a259dbcaca9c078
Aleksandr Yakimenko <37263666+alyakimenko@users.noreply.github.com>
2022-12-08 20:22:57 +0200

    Add database and collection validations (#18)

---
+++ common/collection.go
@@ -0,0 +1,57 @@
+// Copyright © 2022 Meroxa, Inc. & Yalantis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package common provides functions shared between different parts of the connector.
+package common
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// nameFilterName is a filter name that is used for querying database and collection names.
+const nameFilterName = "name"
+
+// GetMongoCollection checks if the provided database and collection
+// exist in a Mongo instance the client is connected to, and returns the [mongo.Collection] if they exist.
+// By default, the Go Mongo driver creates a database and collection if they don't exist,
+// so this function may come in handy when it comes to validations.
+func GetMongoCollection(ctx context.Context, client *mongo.Client, db, collection string) (*mongo.Collection, error) {
+	// list database names filtering them by name, so we must get only one database name if it exists
+	databaseNames, err := client.ListDatabaseNames(ctx, bson.M{nameFilterName: db})
+	if err != nil {
+		return nil, fmt.Errorf("list database names: %w", err)
+	}
+
+	// if the list operation returns nothing - the database doesn't exist
+	if len(databaseNames) == 0 {
+		return nil, fmt.Errorf("database %q doesn't exist", db)
+	}
+
+	// list collection names filtering them by name, so we must get only one collection name if it exists
+	collectionNames, err := client.Database(db).ListCollectionNames(ctx, bson.M{nameFilterName: collection})
+	if err != nil {
+		return nil, fmt.Errorf("list collection names: %w", err)
+	}
+
+	// if the list operation returns nothing - the collection doesn't exist
+	if len(collectionNames) == 0 {
+		return nil, fmt.Errorf("collection %q doesn't exist", collection)
+	}
+
+	return client.Database(db).Collection(collection), nil
+}
